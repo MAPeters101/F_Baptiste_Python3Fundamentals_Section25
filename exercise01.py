39b04c78bb9b4a0f2b74448c6d00483bd8b9b562
Mark Peters <mapeters101@earthlink.net>
2025-03-12 23:50:23 -0400

    Descriptions for exercise01.

---
+++ exercise01.py
@@ -0,0 +1,12 @@
+'''
+Question 1
+There is a file named transactions.csv which is a list of purchases and sales.
+
+Write code that loads this data and calculates the total of these purchases
+and sales.
+
+Take two approaches - one using floats, and one using Decimal objects.
+Calculate the difference between the two results.
+
+Also, time how long it takes to run your code using floats and using Decimals.
+'''
